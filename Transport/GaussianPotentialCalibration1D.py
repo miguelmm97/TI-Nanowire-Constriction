8350e516a55f35860e091846b874238822da7b2d
Miguel <miguelmartinezmiquel@gmail.com>
2023-09-13 15:03:50 +0200

    Solved (I think) the issues with the potential in 1d

--- Transport/GaussianPotentialCalibration1D.py
+++ Transport/GaussianPotentialCalibration1D.py
@@ -9,11 +9,11 @@ start_time = time.time()
 vf           = 330               # Fermi velocity in meV nm
 corr_length  = 10                # Correlation length in nm
 dis_strength = 6                 # Disorder strength in vf / xi scale
-Nq           = 600             # Number of points to take the FFT
-L            = 100               # Length of the wire
+Nq           = 2000              # Number of points to take the FFT
+L            = 1000              # Length of the wire
 rad          = 8                 # Radius of the wire
-Nx           = 500               # Number of points in the x grid
-ncheck       = 200               # Samples of the potential
+Nx           = 600               # Number of points in the x grid
+ncheck       = 2000              # Samples of the potential
 
 # Generate gaussian correlated potential
 x = np.linspace(0, L, Nx)
@@ -21,45 +21,45 @@ r = np.repeat(rad, x.shape[0])
 V = np.zeros((ncheck, x.shape[0]))
 print('dx = {}, pi/q_max= {}'.format(x[1] - x[0], pi * (x[-1] - x[0]) / (2 * pi * Nq)))
 for i in range(ncheck):
-    # print('Sample {}/{}'.format(i, ncheck - 1))
+    print('Sample {}/{}'.format(i, ncheck - 1))
     V[i, :] = gaussian_correlated_potential_1D(x, dis_strength, corr_length, vf, Nq)[0]
 
+# Correlations
+average_full = np.mean(V)
+average_sample = np.mean(V, axis=1)
+Variance = np.zeros((V.shape[0], int(x.shape[0]/2)))
+for i in range(ncheck):
+    aux = np.outer(V[i, :], V[i, :])
+    for j in range(int(x.shape[0]/2)): Variance[i, j] = (np.trace(aux, j) + np.trace(aux, x.shape[0] - j))/(Nx)
+Variance = np.mean(Variance, axis=0)
 
 # Different energy scales
 Vstd_th = np.sqrt(dis_strength / np.sqrt(2 * pi)) * (vf / corr_length)
 Vstd_num = np.std(V, axis=0)[0]
-V_avg = np.mean(V, axis=0)
-
-
-# Correlations
-Variance_aux = np.zeros(V.shape)
-for i in range(ncheck):
-    for j in range(x.shape[0]):
-        aux = V[i, :] * np.roll(V[i, :], -j)
-        if j == 0: Variance_aux[i, j] = np.mean(aux)
-        else: Variance_aux[i, j] = np.mean(aux[0: -j])
-Variance = np.mean(Variance_aux, axis=0)
 
 
 # Fidelity of the sample
-fit = np.polyfit(x[: 5], np.log(Variance[: 5]), 2)
+n_fit = int(np.floor(3 * corr_length / (L / Nx)))
+fit = np.polyfit(x[: n_fit], np.log(Variance[: n_fit]), 2)
 scale_fit = np.exp(fit[2])
 xi_fit = np.sqrt(- (1 / fit[0]) / 2)
+error1 = Vstd_th ** 2 - Variance[0]
+error2 = np.abs(Variance[-1])
 
 
 # Quality of the fit depending on the system size
-# L_vec = np.linspace(100, 2000, 20); dx = 0.2; dq = 0.19; ncheck2 = 200   # dx = 2.04; dq = 1.9;
+# L_vec =np.linspace(100, 1000, 20); dx = 2.5; ncheck2 = 200
 # num_error = np.zeros(L_vec.shape)
 # for i, l in enumerate(L_vec):
 #     print('Length {}/{}'.format(i, L_vec.shape[0]))
 #     Nx = int(np.floor(l / dx))
-#     Nq = int(np.floor(l / (2 * pi * dq)))
+#     Nq = int(np.floor(l / (2 * dx))) + 1
 #
 #     # Generate potential
 #     x2 = np.linspace(0, l, Nx)
 #     V2 = np.zeros((ncheck2, x2.shape[0]))
 #     for j in range(ncheck2):
-#         V2[j, :] = gaussian_correlated_potential_1D(x2, dis_strength, corr_length, vf, Nq)
+#         V2[j, :] = gaussian_correlated_potential_1D(x2, dis_strength, corr_length, vf, Nq)[0]
 #
 #     # Correlations
 #     Variance_aux2 = np.zeros(V2.shape)
@@ -140,14 +140,15 @@ plt.show()
 
 
 # Spacial correlations
-Variance_th = (Vstd_th ** 2) * np.exp(-0.5 * x ** 2 / corr_length ** 2)
+Variance_th = (Vstd_th ** 2) * np.exp(-0.5 * x[: -int(Nx / 2)] ** 2 / corr_length ** 2)
 fig4, ax4 = plt.subplots(figsize=(8, 6))
-ax4.plot(x, Variance, '.r')
-ax4.plot(x, Variance_th, '-b')
+ax4.plot(x[: -int(Nx / 2)], Variance, '.r')
+ax4.plot(x[: -int(Nx / 2)], Variance_th, '-b')
+ax4.plot(x[: -int(Nx / 2)], (Vstd_th ** 2) * np.ones(int(Nx/2)), '--')
 ax4.set_ylabel("$\langle V(x) V(x') \\rangle$ ")
 ax4.set_xlabel("$x$")
 ax4.set_yscale('log')
-ax4.set_ylim(1, 10000)
+ax4.set_ylim(1, 3000)
 ax4.set_xlim(0, 10 * corr_length)
 ax4.set_title("$\langle V(x) V(x') \\rangle$  with $\\xi=$ {} nm, $K_V=$ {} and $N_q=$ {}".format(corr_length, dis_strength, Nq))
 ax4.text(50, 1000, f'$\\xi_{{fit}} / \\xi_{{th}} = {xi_fit / corr_length:.2f}$')
@@ -156,11 +157,10 @@ ax4.text(50, 100, f'$Scale_{{fit}} / Scale_{{th}} = {scale_fit / Vstd_th ** 2:.2
 # Numerical error with system size
 # fig5, ax5 = plt.subplots(figsize=(8, 6))
 # ax5.plot(L_vec, num_error/ (Vstd_th ** 2), '.r')
-# ax5.set_ylabel("$\langle V_t^2 \\rangle - \langle V^2 \\rangle$ ")
+# ax5.set_ylabel("$(\langle V_t^2 \\rangle - \langle V^2 \\rangle) / \langle V_t^2\\rangle$ ")
 # ax5.set_xlabel("$L$")
 # ax5.set_xlim(0, L_vec[-1])
-# ax5.set_title('Numerical error in the variance for $dx = $ {}, $1/q = $ {}'.format(dx, dq))
-
+# ax5.set_title('Numerical error in the variance for $dx = $ {} and $\\xi =$ {}'.format(dx, corr_length))
 
 
 
